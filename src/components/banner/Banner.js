037344a0b72fb82c6660e5b9f54fe8d7ff11a0f0
Bruno Luis Martins <zapelim.dev>
2021-03-20 20:02:17 -0300

    menu-function

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -4,9 +4,9 @@ import './banner.css'
 import Scroll from 'react-scroll'
 
 
-function handleClick(){
+function handleClick(num){
 
-    Scroll.animateScroll.scrollTo(1850)
+    Scroll.animateScroll.scrollTo(num)
 }
 
 function Banner () {
@@ -34,8 +34,8 @@ function Banner () {
                 <h1 className="banner-title">Lorem ipsum dolor sit amet, consectetur adipiscing. </h1>
                 <h3 className="banner-subtitle">Consultores especializados somados a inteligência artificial e atendimento personalizado.</h3>
                 <div className="banner-footer">
-                    <button className="saiba-mais-btn">Saiba Mais</button>
-                    <button className="precos-btn" onClick={() => handleClick("pricing")}>Ver preços</button>
+                    <button className="saiba-mais-btn" onClick={() => handleClick(3000)}>Saiba Mais</button>
+                    <button className="precos-btn" onClick={() => handleClick(1850)}>Ver preços</button>
                 </div>
             </div>
         </div>
