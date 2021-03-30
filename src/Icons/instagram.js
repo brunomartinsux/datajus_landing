c94e3a015deb7741c0d1edae7f1f2201cbdba1cc
Bruno Luis Martins <zapelim.dev>
2021-03-30 16:48:59 -0300

    fix

--- src/Icons/instagram.js
+++ src/Icons/instagram.js
@@ -11,7 +11,7 @@ function IconInstagram (props){
 
     return (
         <div className={'instagram'} onClick={() => HandleIconClick()}> 
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" strokeLinejoin="round" className="feather feather-instagram">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-instagram">
             <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
             <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
             <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
