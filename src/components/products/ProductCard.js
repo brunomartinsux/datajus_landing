496473d8db5fd0382267f4d6b8e522c6eb765761
Bruno Luis Martins <zapelim.dev>
2021-05-21 11:26:13 -0300

    adião de titulo no title

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -7,7 +7,7 @@ function ProductCard (props) {
     return (
         <div className="product-card-container slide-in-left">
             {props.children}
-            
+            <h1 className="product-card-title">{title}</h1>
             <h3>{subtitle}</h3>
         </div>
     )
